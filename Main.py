15cfd306c2ac4b97941b7c827cbc1ab75b65125b
Emmanuel Iloh <emmanueli.work@gmail.com>
2018-04-11 22:45:49 +0800

    tkinter
    experiments with stuff

--- Main.py
+++ Main.py
@@ -1,8 +1,29 @@
 from Voice import audioToText
 from FileIO import readBible
 from Passage import passageFinder
-bible = readBible("ESV")
+from tkinter import *
+import tkinter.messagebox
 
-passage = audioToText()
+def getText():
+    global w
+    passage = w.get()
+    return passage
+
+
+bible = readBible("ESV")
+root = Tk()
+
+topFrame = Frame(root, width = 300, height = 500)
+topFrame.pack()
+
+w = Entry(topFrame)
+voiceButton = Button(topFrame, text = "Voice Search", fg = "blue")
+textButton = Button(topFrame, text = "Text Search", fg = "red", command = getText)
+w.grid(row = 2, column = 1)
+voiceButton.grid(row = 1, column = 1)
+textButton.grid(row = 3, column = 1)
+#passage = "James 5 vs 1 to 5"
 print (passage)
-print(passageFinder(bible, passage))
+#print(passageFinder(bible, passage))
+root.mainloop()
+#passage = audioToText()
